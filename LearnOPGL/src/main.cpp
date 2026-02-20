8abf879702b926d55115ff8f2ca706921e43f44b
Juma Al Remeithi <jumaadbusiness@gmail.com>
2026-02-20 17:57:31 +0400

    Initial working starter window, (GLAD, GLFW, OPGL x64, MSVC//VS2022)

---
+++ LearnOPGL/src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>	
+#include <glad/glad.h>  // GLAD goes first!
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Make a blue screen */
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
