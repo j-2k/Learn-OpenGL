0db339ec05a012d3dead18df3d577a214870a221
Juma Al Remeithi <jumaadbusiness@gmail.com>
2026-02-21 22:19:00 +0400

    Part 1: Window Creation, Reading input, Window Resize Handling, Testing GLFW/GLAD

--- LearnOPGL/src/main.cpp
+++ LearnOPGL/src/main.cpp
@@ -1,48 +1,88 @@
-#include <iostream>	
 #include <glad/glad.h>  // GLAD goes first!
 #include <GLFW/glfw3.h>
 
-int main(void)
+#include <iostream>	
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Engine Consts
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+
+int main()
 {
-    GLFWwindow* window;
+	std::cout << "Hello, OpenGL!" << std::endl;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	glfwInit();
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	//OpenGL version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	//Core profile = No backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	//Window creation
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OnlyClown", NULL, NULL);
+	if(window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	//callback for window resizing
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-        /* Make a blue screen */
-		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	//(Initialize GLAD) GLAD: load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	//Render loop
+	while (!glfwWindowShouldClose(window)) {
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		processInput(window);
 
-    glfwTerminate();
-    return 0;
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//cleanup and exit
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	//Print if WASD keys are pressed
+	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		std::cout << "W";
+	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		std::cout << "A";
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		std::cout << "S";
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		std::cout << "D";
+
+
+}
+
+// (GLFW) Whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
