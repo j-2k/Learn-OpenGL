c21628e2c5040a20fa9a4c2b4274dac0a5e06bfa
Juma Al Remeithi <jumaadbusiness@gmail.com>
2026-02-21 22:22:20 +0400

    Blue background!

--- LearnOPGL/src/main.cpp
+++ LearnOPGL/src/main.cpp
@@ -50,6 +50,9 @@ int main()
 
 		processInput(window);
 
+		glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
