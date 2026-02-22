3dcd4204377bb95bd7f185ed606d0d0f19200be8
Juma Al Remeithi <jumaadbusiness@gmail.com>
2026-02-22 18:35:38 +0400

    Making my first triangle, brb:)

--- LearnOPGL/src/main.cpp
+++ LearnOPGL/src/main.cpp
@@ -10,6 +10,27 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// Window Title
+const char* WINDOW_TITLE = "CircusClown";
+
+
+// Vertex Shader
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+// Fragment Shader
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
 
 
 int main()
@@ -26,7 +47,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//Window creation
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OnlyClown", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, WINDOW_TITLE, NULL, NULL);
 	if(window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -44,6 +65,61 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	//First Triangle Steps ==========================================
+
+	//1. Vertex shader creation stage ---------------------------------
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	//Attach shader source code to the shader object and compile the shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Check for shader compile errors
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//Vertex shader creation end ---------------------------------
+
+	//2. Fragment shader creation stage ---------------------------------
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//Attach shader source code to the shader object and compile the shader
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//Check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//Fragment shader creation end ---------------------------------
+
+	//3. Shader Program & Linking Stage ---------------------------------
+
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	//Generate Vertex Array Object and bind it
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Copy our vertices array in a buffer for OpenGL to use
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
