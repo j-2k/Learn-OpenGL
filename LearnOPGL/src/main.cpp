e4b57b0a6086d1b89026b04c2bf74c2a39a32c3e
Juma Al Remeithi <jumaadbusiness@gmail.com>
2026-02-27 18:26:25 +0400

    Buffers setup for textures only & did some file restructuring, Iftaar timeee

--- LearnOPGL/src/main.cpp
+++ LearnOPGL/src/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>	
 
 #include "./shader-utils/shaderLoaders.h"
+#include "../external/stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -66,31 +67,62 @@ int main()
 		return -1;
 	}
 
+	//Texture Loading
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	//After we're done generating the texture and its corresponding mipmaps, it is good practice to free the image memory
+	stbi_image_free(data);
+
+
 
 
 	// Setup vertices and buffers and configure vertex attributes ---------------------------------
-	/*
+
+	//Using EBO to to reduce verts for multiple tris
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		//Position          // Color		  //Texture coords
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.f, 1.f,		// top right
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.f, 0.f,		// bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.f, 0.f,		// bottom left
+		-0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.f, 1.f,		// top left 
 	};
 	unsigned int indices[] = {  // note that we start from 0!
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
 	};
-	*/
 
+
+	/* //triangles without EBO
 	float vertices[] = {
 		// positions         // colors
 		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
 		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
 		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
+	*/
 
 	unsigned int VBO, VAO, EBO;
-	//glGenBuffers(1, &EBO);			// reserve an EBO ID
+	glGenBuffers(1, &EBO);			// reserve an EBO ID
 	glGenVertexArrays(1, &VAO);		// reserve a VAO ID
 	glGenBuffers(1, &VBO);			// reserve a VBO ID
 
@@ -98,14 +130,20 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);												// select VBO as the active buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// upload vertex data to GPU
 	
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);	// select EBO as the active buffer
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);	// upload vertex data to GPU
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// describe layout: slot 0, 3 floats, stride 12 bytes, offset 0
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);	// select EBO as the active buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);	// upload vertex data to GPU
+
+	//glVertexAttribPointer (SLOT, SIZE, TYPE, NORMALIZED, STRIDE, OFFSET) <<< this is how we tell OpenGL how to interpret the vertex data we just uploaded. We have to do this for each attribute in our vertex data (position, color, texture coords)
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);	// POSITION layout: slot 0, 3 floats, stride 12 bytes, offset 0
 	glEnableVertexAttribArray(0);	// enable attribute slot 0 so the shader can read it
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));	// layout is now : slot 1, 3 floats, stride 12 bytes, offset 12 bytes (after the position data)
-	glEnableVertexAttribArray(1);	// enable attribute slot 0 so the shader can read it
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));	// COLOR layout is now : slot 1, 3 floats, stride 12 bytes, offset 12 bytes (after the position data)
+	glEnableVertexAttribArray(1);	// enable attribute slot 1 so the shader can read it
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));	// TEXTURE layout is now : slot 2, 2 floats, stride 8 bytes, offset 12 bytes (after the color data)
+	glEnableVertexAttribArray(2);	// enable attribute slot 2 so the shader can read it
+
 	glBindVertexArray(0);			// stop recording, VAO is saved
 
 	//Since we have 1 shader with the rgb verts that is constant we just set it once outisde the loop.
@@ -124,8 +162,8 @@ int main()
 
 		//Draw our first triangle
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // using EBO instead of VBO for glDrawElements
+		//glDrawArrays(GL_TRIANGLES, 0, 3); //triangles without EBO
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // using EBO instead of VBO for glDrawElements
 		glBindVertexArray(0); // no need to unbind it every time
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
